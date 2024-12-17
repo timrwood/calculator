9a1a37497f459886254bfed5d7767ccbc2e64307
Tim Wood <washwithcare@gmail.com>
2024-12-17 09:39:44 -0600

    Align and fade in

--- app/components/interpreter-preview.ts
+++ app/components/interpreter-preview.ts
@@ -11,4 +11,43 @@ export interface InterpreterPreviewSignature {
   Element: null
 }
 
-export default class InterpreterPreview extends Component<InterpreterPreviewSignature> {}
+function longestString(strings: string[]) {
+  let max = ''
+  for (let i = 0; i < strings.length; i++) {
+    const string = strings[i]
+    if (string && string.length > max.length) {
+      max = strings[i] as string
+    }
+  }
+  return max
+}
+
+export default class InterpreterPreview extends Component<InterpreterPreviewSignature> {
+  get maxLine() {
+    return longestString(this.args.interpreter.evaluations.map(evaluation => `L${evaluation.step}`))
+  }
+
+  get maxVisualsCommand() {
+    const strings = this.args.interpreter.evaluations
+      .flatMap(evaluation => evaluation.visuals)
+      .map(visual => `${visual.op} ${visual.ref}`)
+    return longestString(strings)
+  }
+
+  get maxVisualsValue() {
+    const strings = this.args.interpreter.evaluations
+      .flatMap(evaluation => evaluation.visuals)
+      .map(visual => `= ${visual.val}`)
+    return longestString(strings)
+  }
+
+  get maxCommandValue() {
+    const strings = this.args.interpreter.evaluations.map(evaluation => evaluation.src)
+    return longestString(strings)
+  }
+
+  get maxRefValue() {
+    const strings = this.args.interpreter.program.refs.map(ref => `${ref} = `)
+    return longestString(strings)
+  }
+}
